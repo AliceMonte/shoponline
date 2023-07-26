478fe324bd3b711e01bf974849857a515bda12a0
AliceMonte <lol.gerda96@gmail.com>
2023-07-26 20:46:55 +0300

    157

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 
 
 @app.route('/')
 def index():
-    return 'Начало сайта'
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
